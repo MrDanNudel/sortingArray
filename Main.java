6b7fa57083def296d9c953fd278dac2d584e2bf3
Dan nudelman <167123191+MrDanNudel@users.noreply.github.com>
2024-05-20 18:38:51 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] numbers = {5, 2, 8, 3, 1, 4, 7, 6};
+        System.out.println("Original array: " + java.util.Arrays.toString(numbers));
+
+        sortArray(numbers);
+        System.out.println("Sorted array: " + java.util.Arrays.toString(numbers));
+    }
+    public static void sortArray(int[] array) {
+        int n = array.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+               for (int j = 0; j < n - 1 - i; j++) {
+                // Swap if the element found is greater than the next element
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // If no elements were swapped, the array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
     }
 }
+
+
+
+
+
+
+
